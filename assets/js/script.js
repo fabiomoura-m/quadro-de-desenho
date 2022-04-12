a03c52840b7cfd831ef5cdea257af5ba2f2e6b05
Fabio de Moura Martins <fabio.mmouras@hotmail.com>
2022-04-12 08:46:41 -0300

    feat: inclui eventos de mouse que monitoram o canvas

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,21 @@
+// Initial Data
 let currentColor = 'black';
+let canDraw = false;
+let mouseX = 0;
+let mouseY = 0;
 
+let screen = document.querySelector('#tela');
+let context = screen.getContext('2d');
+
+//Events
 document.querySelectorAll('.colorArea .color').forEach(item => {
   item.addEventListener('click', colorClickEvent);
 });
+screen.addEventListener('mousedown', mouseDownEvent);
+screen.addEventListener('mousemove', mouseMoveEvent);
+screen.addEventListener('mouseup', mouseUpEvent);
 
+//Functions
 function colorClickEvent(e) {
   let color = e.target.getAttribute('data-color');
   currentColor = color;
@@ -11,3 +23,23 @@ function colorClickEvent(e) {
   document.querySelector('.color.active').classList.remove('active');
   e.target.classList.add('active');
 }
+function mouseDownEvent(e) {
+  canDraw = true;
+  mouseX = e.pageX - screen.offsetLeft;
+  mouseY = e.pageY - screen.offsetTop;
+}
+function mouseMoveEvent(e) {
+  if (canDraw) {
+    draw(e.pageX, e.pageY);
+  }
+}
+function mouseUpEvent() {
+  canDraw = false;
+}
+
+/*
+Passo a passo para desenhar no canvas:
+-Quando o click do mouse ABAIXAR, ative o modo desenho.
+-Quando o mouse se MOVER, se o modo desenho estiver ativado, desenhe.
+-Quando o click do mouse LEVANTAR, desative o modo desenho.
+*/
